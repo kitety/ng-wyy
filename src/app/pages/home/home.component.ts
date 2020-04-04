88ccd5e067b15e7870d8338e08e0690dad374c7a
kitety <aizaizuori@foxmail.com>
2020-04-04 14:51:58 +0800

    feat: 7组件轮播组件

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from 'src/app/services/home.service';
+import { Banner } from 'src/app/services/data-types/common.types';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,10 @@ import { HomeService } from 'src/app/services/home.service';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-
+  banners: Banner[]
   constructor(private homeService: HomeService) {
     this.homeService.getBanners().subscribe(banners => {
-      console.log('banners: ', banners);
-
+      this.banners = banners
     })
   }
 
